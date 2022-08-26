04a68dc916ba4ff0a005f96483e4cfdcd6c3e4d6
SofiiaKor <SofiiaKor@github.com>
2022-08-26 22:55:04 +0300

    Added some unit tests

---
+++ KorzhynskaSofiia.RobotChallenge.Test/TestAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using KorzhynskaSofiia.RobotChallange;
+using Robot.Common;
+
+namespace KorzhynskaSofiia.RobotChallenge.Test
+{
+	[TestClass]
+	public class TestAlgorithm
+	{
+		[TestMethod]
+		public void TestMoveCommand()
+		{
+			var algorithm = new KorzhynskaSofiiaAlgorithm();
+			var robots = new List<Robot.Common.Robot>()
+				{new Robot.Common.Robot() {Energy = 200, Position = new Position(2, 3)}};
+			var map = new Map();
+			var stationPosition = new Position(1, 1);
+			map.Stations.Add(new EnergyStation() { Energy = 1000, Position = stationPosition, RecoveryRate = 2 });
+
+			var command = algorithm.DoStep(robots, 0, map);
+
+			Assert.IsTrue(command is MoveCommand);
+			Assert.AreEqual(((MoveCommand)command).NewPosition, stationPosition);
+		}
+
+		[TestMethod]
+		public void TestCollectCommand()
+		{
+			var algorithm = new KorzhynskaSofiiaAlgorithm();
+			var robots = new List<Robot.Common.Robot>()
+				{new Robot.Common.Robot() {Energy = 200, Position = new Position(1, 1)}};
+			var map = new Map();
+			var stationPosition = new Position(1, 1);
+			map.Stations.Add(new EnergyStation() { Energy = 1000, Position = stationPosition, RecoveryRate = 2 });
+
+			var command = algorithm.DoStep(robots, 0, map);
+
+			Assert.IsTrue(command is CollectEnergyCommand);
+		}
+	}
+}
---
+++ KorzhynskaSofiia.RobotChallenge.Test/TestIsCellFree.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using KorzhynskaSofiia.RobotChallange;
+using Robot.Common;
+
+namespace KorzhynskaSofiia.RobotChallenge.Test
+{
+	[TestClass]
+	public class TestIsCellFree
+	{
+		[TestMethod]
+		public void TestCell()
+		{
+			var test = new KorzhynskaSofiiaAlgorithm();
+			var cell = new Position(5, 5);
+			var movingRobot = new Robot.Common.Robot() {Position = new Position(10, 10)};
+			var robots = new List<Robot.Common.Robot>()
+				{new Robot.Common.Robot() {Energy = 200, Position = new Position(2, 3)}};
+
+			Assert.IsTrue(test.IsCellFree(cell, movingRobot, robots));
+		}
+	}
+}
